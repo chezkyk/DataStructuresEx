10bd1b250918f16d588c5ebd8c09b24e01490976
chezkyk <Y0548532123@GMAIL.COM>
2024-09-04 11:29:32 +0300

    I copied from other projects that I already had, and in order to keep order, I am combining them into one project.

---
+++ Complexities.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructuresEx
+{
+    public class Complexities
+    {
+        //ex 1
+        public static int CalculateOddSum(int[] array)
+        {
+            int sum = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] % 2 != 0)
+                {
+                    sum += array[i];
+                }
+            }
+            return sum;
+        }
+
+        //ex 2
+        public static int[] FindMaximumTwo(int[] array)
+        {
+            int[] towLargestInArray = new int[2];
+            int sum = array[0] + array[1];
+            int max1 = array[0];
+            int max2 = array[1];
+            for (int i = 1; i < array.Length - 1; i++)
+            {
+                if (array[i] + array[i + 1] > sum)
+                {
+                    max1 = array[i];
+                    max2 = array[i + 1];
+                    sum += max1 + max2;
+                }
+            }
+            towLargestInArray[0] = max1;
+            towLargestInArray[1] = max2;
+            return towLargestInArray;
+        }
+
+        // ex 3
+        public static bool ContainsNumber(int[] array, int number)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] == number)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        // ex 4
+        public static int[] SumOfAllTriplets(int[] array)
+        {
+            int[] helpArr = new int[3];
+            int first = array[0];
+            int second = array[1];
+            int third = array[2];
+            int sum = first + second + third;
+            int temp = 0;
+            for (int i = 1; i < array.Length - 2; i++)
+            {
+                temp = array[i] + array[i + 1] + array[i + 2];
+                if (temp > sum)
+                {
+                    first = array[i];
+                    second = array[i + 1];
+                    third = array[i + 2];
+                    sum = first + second + third;
+                }
+            }
+            helpArr[0] = first;
+            helpArr[1] = second;
+            helpArr[2] = third;
+            return helpArr;
+        }
+
+        // ex 5
+        public static int[] FindLastClosePairWithProduct(int[] array, int product)
+        {
+            int[] productArr = new int[2];
+            int first = 0;
+            int second = 0;
+            int sumProduct = 0;
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                sumProduct = array[i] * array[i + 1];
+                if (sumProduct == product)
+                {
+                    first = array[i];
+                    second = array[i + 1];
+                }
+            }
+            productArr[0] = first;
+            productArr[1] = second;
+            return productArr;
+        }
+
+        // ex 6
+        public static int CountEvenNumbers(int[] array)
+        {
+            int count = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] % 2 == 0)
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+
+        // ex 7
+        public static int[] MergeSortedArraysUnique(int[] array1, int[] array2)
+        {
+            
+        }
+    }
+}
---
+++ Recursion.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructuresEx
+{
+    internal class Recursion
+    {
+        //ex 1
+        public static int Multiply(int a, int b)
+        {
+            if (a == 0)
+            {
+                return a;
+            }
+            return b + Multiply(a - 1, b);
+        }
+
+        //ex 2
+        public static int SumOfDigits(int n)
+        {
+            if (n < 10)
+            {
+                return n;
+            }
+            return n % 10 + SumOfDigits(n / 10);
+        }
+
+        //ex 3
+        public static int Power(int a, int b)
+        {
+            if (b == 1)
+            {
+                return a;
+            }
+            return a * Power(a, b - 1);
+        }
+
+        //ex 4
+        public static string Reversed(int n)
+        {
+            if (n < 10)
+            {
+                return n.ToString();
+            }
+            return (n % 10).ToString() + Reversed(n / 10);
+        }
+
+        //ex 5
+        public static int FindSum(int[] n, int len)
+        {
+            if (len == 0)
+            {
+                return len;
+            }
+            return n[len - 1] + FindSum(n, len - 1);
+        }
+
+        // ex 6 
+        public static float Divide(float a, float b)
+        {
+
+            if (a < b)
+            {
+                return 0;
+            }
+            return 1 + Divide(a - b, b);
+        }
+
+        //ex 7
+        public static bool IsPalindrome(string str)
+        {
+            
+        }
+    }
+}
+
