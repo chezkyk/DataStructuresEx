73f82db8cc7c249d1f98c0f51769b6789ebf1c83
chezkyk <Y0548532123@GMAIL.COM>
2024-09-04 12:23:50 +0300

    I implemented the following methods in the stack; Push, Pop, Peak, Count.

--- Stack.cs
+++ Stack.cs
@@ -6,7 +6,51 @@ using System.Threading.Tasks;
 
 namespace DataStructuresEx
 {
-    internal class Stack
+    public class Stack
     {
+        public Node Top;
+        public int count;
+        public Stack() { }
+        public Stack(int data)
+        {
+            Top = new Node(data);
+        }
+        public void Push(int data)
+        {
+            Node newNode = new Node(data);
+            if (Top == null)
+            {
+                Top = newNode;
+            }
+            else
+            {
+                newNode.Next = Top;
+                Top = newNode;
+            }
+            count++;
+        }
+        public void Pop()
+        {
+            if (Top == null)
+            {
+                return;
+            }
+            Top = Top.Next;
+        }
+        public int Peak()
+        {
+            Node node = Top;
+            if (node == null)
+            {
+                return -1;
+            }
+            return node.GetValue();
+        }
+        public int Count()
+        {
+            return count;
+        }
     }
+
+
 }
